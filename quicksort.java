6033221aa6fda28b10ffa1b954acbd420a02117b
Vinay D <114236808+VinayD1382@users.noreply.github.com>
2024-09-10 21:36:22 +0530

    Update quicksort.java

--- quicksort.java
+++ quicksort.java
@@ -29,7 +29,7 @@ public class quicksort {
 				i++;
 				int temp = arr[j];
 				arr[j]=arr[i];
-				arr[i] = temp;
+				arr[i] = temp;         //{4,5,6,8,9,,1,2,3};
 			}
 		}
 		int tempu = arr[i+1];
