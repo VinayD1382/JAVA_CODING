76af24eb8cae94b28904e5f3bb04dbbae5746a64
Vinay D <114236808+VinayD1382@users.noreply.github.com>
2024-12-09 19:40:37 +0530

    Update simpleregularexpression.java

--- simpleregularexpression.java
+++ simpleregularexpression.java
@@ -4,7 +4,7 @@ public class simpleregularexpression {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str = "[0-9]+";
+		String str = "[0-9]+"; // 2 cheks in [0-9] , + is then 3 checks in [0-9],+ 5 in [0-9] , [0-9],[0-9],[0-9].... in search of number.
 		String str1 = "23569874";
 			System.out.println("Str1 is found in str : " + str1.matches(str));
 
